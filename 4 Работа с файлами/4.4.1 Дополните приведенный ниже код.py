ee5dc441d522c34722a2e36fac8fe804d2fdfde9
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2023-04-17 19:26:09 +0700

    feat: add 4.4.1 - 4.4.5 solutions

---
+++ 4 Работа с файлами/4.4.1 Дополните приведенный ниже код.py
@@ -0,0 +1,14 @@
+# Дополните приведенный ниже код, чтобы он вывел содержимое словаря countries, расположив его элементы в
+# лексикографическом порядке ключей, указав в качестве разделителя пар ключ-значение строку   -  (пробел дефис пробел),
+# а в качестве отступов — три пробела.
+
+import json
+
+countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
+             'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
+             'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
+             'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
+
+json_countries = json.dumps(countries, indent=3, separators=(',', ' - '), sort_keys=True)
+
+print(json_countries)
---
+++ 4 Работа с файлами/4.4.5 Функция is_correct_json.py
@@ -0,0 +1,15 @@
+# Функция is_correct_json()
+# Реализуйте функцию is_correct_json(), которая принимает один аргумент:
+#
+# string — произвольная строка
+# Функция должна возвращать True, если строка string удовлетворяет формату JSON, или False в противном случае.
+
+import json
+
+
+def is_correct_json(string):
+    try:
+        data_json = json.loads(string)
+        return True
+    except:
+        return False
